573a8298c7ea828e986c9654fe9676746065fd35
wancong <wancongzhang@gmail.com>
2020-08-06 11:11:29 -0400

    'd'

---
+++ train.py
@@ -0,0 +1,106 @@
+import pdb
+
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import ZeroPadding2D,Convolution2D,MaxPooling2D
+from tensorflow.keras.layers import Dense,Dropout,Softmax,Flatten,Activation,BatchNormalization
+from tensorflow.keras.preprocessing.image import load_img,img_to_array
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+import tensorflow.keras.backend as K
+
+#Define VGG_FACE_MODEL architecture
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+model.add(Convolution2D(4096, (7, 7), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(4096, (1, 1), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Convolution2D(2622, (1, 1)))
+model.add(Flatten())
+model.add(Activation('softmax'))
+
+# Load VGG Face model weights
+model.load_weights('vgg_face_weights.h5')
+
+# Remove Last Softmax layer and get model upto last flatten layer with outputs 2622 units
+vgg_face=Model(inputs=model.layers[0].input,outputs=model.layers[-2].output)
+
+# Construct folder names
+folders_train = []
+folders_test = []
+
+for i in range(1,71):
+    female = str(i) + '_F'
+    male = str(i) + '_M'
+    folders_train += [female, male]
+    folders_test += [female, male]
+
+# Get face embeddings
+
+def get_embedding(file_path):
+  img = load_img(file_path, target_size=(224,224))
+  img=img_to_array(img)
+  img=np.expand_dims(img,axis=0)
+  img=preprocess_input(img)
+  img_encode=vgg_face(img)
+  return np.squeeze(K.eval(img_encode)).tolist()
+
+def process_folders(folders, root, x, y):
+  for folder in folders:
+    if folder[-1] == 'F':
+      gender = 0
+    else:
+      gender = 1
+
+    folder = root + '/' + folder
+    for filename in os.listdir(folder):
+      file_path = folder + '/' + filename
+      embedding = get_embedding(file_path)
+      x.append(embedding)
+      y.append(gender)
+
+folders_train = ['01_F', '01_M']
+root_train = '/combined/aligned'
+
+x_train = []
+y_train = []
+
+process_folders(folders_train, root_train, x_train, y_train)
+
+pdb.set_trace()
+
+exit = "exit"
