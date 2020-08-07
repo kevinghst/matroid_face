4c7ccef0568a14a41404e1b7724592da16ed945c
wancong <wancongzhang@gmail.com>
2020-08-07 10:42:48 -0400

    'd'

---
+++ train.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential,Model
+from tensorflow.keras.layers import Dense,Dropout,Softmax,Flatten,Activation,BatchNormalization
+
+# Load saved data
+x_train=np.load('train_data.npy')
+y_train=np.load('train_labels.npy')
+x_test=np.load('test_data.npy')
+y_test=np.load('test_labels.npy')
+
+# Softmax regressor to classify images based on encoding 
+classifier_model=Sequential()
+classifier_model.add(Dense(units=100,input_dim=x_train.shape[1],kernel_initializer='glorot_uniform'))
+classifier_model.add(BatchNormalization())
+classifier_model.add(Activation('tanh'))
+classifier_model.add(Dropout(0.3))
+classifier_model.add(Dense(units=10,kernel_initializer='glorot_uniform'))
+classifier_model.add(BatchNormalization())
+classifier_model.add(Activation('tanh'))
+classifier_model.add(Dropout(0.2))
+classifier_model.add(Dense(units=6,kernel_initializer='he_uniform'))
+classifier_model.add(Activation('softmax'))
+classifier_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(),optimizer='nadam',metrics=['accuracy'])
+
+# Train model
+classifier_model.fit(x_train,y_train,epochs=10,validation_data=(x_test,y_test))
